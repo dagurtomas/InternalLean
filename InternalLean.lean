702eeecb4fe66f4aec275f7147d811fb7cd2cbb3
Dagur Asgeirsson <dagurtomas@gmail.com>
2026-05-25 13:17:52 -0600

    remove copyright headers

--- InternalLean.lean
+++ InternalLean.lean
@@ -1,8 +1,3 @@
-/-
-Copyright (c) 2026 Dagur Asgeirsson. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Dagur Asgeirsson
--/
 -- Root module for the `InternalLean` library.
 import InternalLean.Basic
 import InternalLean.Command
